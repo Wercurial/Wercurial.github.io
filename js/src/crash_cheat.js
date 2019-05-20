f66780c34902378efbe77792e0cc9fda0f8566b8
Wercurial <wjp_199502@163.com>
2019-05-20 09:04:00 +0800

    Site updated: 2019-05-20 09:03:54

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/images/favicon.ico");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         document.title = '(^_−)☆欢迎下次光临';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/images/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         document.title = 'ヾ(๑╹◡╹)ﾉ客官您又回来啦' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
